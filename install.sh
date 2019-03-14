4b5d357fc479b0a78e182c0c85a4b3c3d6d69309
yuma iwamoto <yuma@bbtower.cloud>
2019-03-14 16:37:33 +0900

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-sudo apt remote docker docker-engine docker.io
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-isudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y 
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
@@ -8,7 +13,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-cache madison docker-ce
 sudo docker run hello-world
-sudo groupadd docker
-sudo usermod -aG docker $USER
+
