f4bd1a97c997c9a4bbbf8fa83742862f6cbea043
yuma iwamoto <yuma@bbtower.cloud>
2019-04-23 15:08:51 +0900

    update

---
+++ docker-compose-install.sh
@@ -0,0 +1,3 @@
+sudo -i
+curl -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
